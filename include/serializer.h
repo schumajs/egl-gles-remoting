c2ed821d03cedef79a55938edd40b0d72065e045
Jens Schumann <schumajs@gmail.com>
2012-01-11 16:18:05 +0100

    Added naive EGL client stub implementation

--- include/serializer.h
+++ include/serializer.h
@@ -22,16 +22,43 @@
 typedef int GVSERcmdid;
 typedef int GVSERcallid;
 
-#define GVSER_MMGR_ALLOC      0
-#define GVSER_MMGR_FREE       1
+#define GVSER_MMGR_ALLOC                         0
+#define GVSER_MMGR_FREE                          1
 
-#define GVSER_SRV_HANDSHAKE   0
+#define GVSER_SRV_HANDSHAKE                      0
 
-#define GVSER_EGL_GETERROR    0
-#define GVSER_EGL_GETDISPLAY  1
-#define GVSER_EGL_INITIALIZE  2
-#define GVSER_EGL_TERMINATE   3
-#define GVSER_EGL_QUERYSTRING 4
-#define GVSER_EGL_GETCONFIGS  5
+#define GVSER_EGL_GETERROR                       0
+#define GVSER_EGL_GETDISPLAY                     1
+#define GVSER_EGL_INITIALIZE                     2
+#define GVSER_EGL_TERMINATE                      3
+#define GVSER_EGL_QUERYSTRING                    4
+#define GVSER_EGL_GETCONFIGS                     5
+#define GVSER_EGL_CHOOSECONFIG                   6
+#define GVSER_EGL_GETCONFIGATTRIB                7
+#define GVSER_EGL_CREATEWINDOWSURFACE            8
+#define GVSER_EGL_CREATEPBUFFERSURFACE           9
+#define GVSER_EGL_CREATEPIXMAPSURFACE           10
+#define GVSER_EGL_DESTROYSURFACE                11
+#define GVSER_EGL_QUERYSURFACE                  12
+#define GVSER_EGL_BINDAPI                       13
+#define GVSER_EGL_QUERYAPI                      14
+#define GVSER_EGL_WAITCLIENT                    15
+#define GVSER_EGL_RELEASETHREAD                 16
+#define GVSER_EGL_CREATEPBUFFERFROMCLIENTBUFFER 17
+#define GVSER_EGL_SURFACEATTRIB                 18
+#define GVSER_EGL_BINDTEXIMAGE                  19
+#define GVSER_EGL_RELEASETEXIMAGE               20
+#define GVSER_EGL_SWAPINTERVAL                  21
+#define GVSER_EGL_CREATECONTEXT                 22
+#define GVSER_EGL_DESTROYCONTEXT                23
+#define GVSER_EGL_MAKECURRENT                   24
+#define GVSER_EGL_GETCURRENTCONTEXT             25
+#define GVSER_EGL_GETCURRENTSURFACE             26
+#define GVSER_EGL_GETCURRENTDISPLAY             27
+#define GVSER_EGL_QUERYCONTEXT                  28
+#define GVSER_EGL_WAITGL                        29
+#define GVSER_EGL_WAITNATIVE                    30
+#define GVSER_EGL_SWAPBUFFERS                   31
+#define GVSER_EGL_COPYBUFFERS                   32
 
 #endif /* SERIALIZER_H */
--- src/client/egl_gles.0.c
+++ src/client/egl_gles.0.c
@@ -119,6 +119,21 @@ initEglGles()
 	}					\
     } while (0)
 
+/* NOTE: attribList is terminated with EGL_NONE, so assert: min.
+   attribListSize == 1
+*/
+#define getAttribListSize(attrbList, attribListSize)		\
+    do {							\
+	attribListSize = 0;					\
+	if (attribList != NULL)					\
+	{							\
+	    while (attribList[attribListSize] != EGL_NONE)	\
+	    {							\
+		attribListSize++;				\
+	    }							\
+	}							\
+    } while (0)
+
 EGLint
 eglGetError()
 {
@@ -170,8 +185,8 @@ eglInitialize(EGLDisplay display, EGLint *major, EGLint *minor)
 
     gvserReturn(callId);
     gvserOutData(&status, sizeof(EGLBoolean));
-    gvserOutData(major, sizeof(EGLint));
-    gvserOutData(minor, sizeof(EGLint));
+    gvserOutData(major,   sizeof(EGLint));
+    gvserOutData(minor,   sizeof(EGLint));
     gvserEndReturn();
 
     return status;
@@ -205,17 +220,21 @@ const char
 {
     GVSERcallid  callId;
     char        *queryString;
-    int          queryStringLength;
+    size_t       queryStringLength;
 
     initIfNotDoneAlready();
 
     callId = gvserCall(GVSER_EGL_QUERYSTRING);
     gvserInData(&display, sizeof(EGLDisplay));
-    gvserInData(&name, sizeof(EGLint));
+    gvserInData(&name,    sizeof(EGLint));
     gvserEndCall();
 
     gvserReturn(callId);
-    gvserOutData(queryString, sizeof(char));
+    /* Read string length: strlen(queryString) + 1 */
+    gvserOutData(&queryStringLength, sizeof(size_t));
+    /* Read actual string */
+    queryString = malloc(queryStringLength * sizeof(char));
+    gvserOutData(queryString, queryStringLength);
     gvserEndReturn();
 
     return queryString;
@@ -224,5 +243,596 @@ const char
 EGLBoolean
 eglGetConfigs(EGLDisplay display, EGLConfig *configs, EGLint configSize, EGLint *numConfig)
 {
-    return 1;
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_GETCONFIGS);
+    gvserInData(&display,    sizeof(EGLDisplay));
+    gvserInData(&configSize, sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status,   sizeof(EGLBoolean));
+    gvserOutData(numConfig, sizeof(EGLint));
+    /* NOTE configs has to be preallocated. If configs == NULL then no configs
+     * are returned, only numConfig is returned (see spec. p. 23)
+     */
+    if (configs != NULL)
+    {
+	gvserOutData(configs, *numConfig * sizeof(EGLConfig));
+    }
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLBoolean
+eglChooseConfig(EGLDisplay display, const EGLint *attribList, EGLConfig *configs, EGLint configSize, EGLint *numConfig)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    EGLint attribListSize;
+    getAttribListSize(attribList, attribListSize);
+
+    callId = gvserCall(GVSER_EGL_CHOOSECONFIG);
+    gvserInData(&display,    sizeof(EGLDisplay));
+    gvserInData(attribList,  attribListSize * sizeof(EGLint));
+    gvserInData(&configSize, sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status,   sizeof(EGLBoolean));
+    gvserOutData(numConfig, sizeof(EGLint));
+    /* NOTE configs has to be preallocated, configs == NULL undefined ... */
+    if (configs != NULL)
+    {
+	gvserOutData(configs, *numConfig * sizeof(EGLConfig));
+    }
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLBoolean
+eglGetConfigAttrib(EGLDisplay display, EGLConfig config, EGLint attribute, EGLint *value)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_GETCONFIGATTRIB);    
+    gvserInData(&display,   sizeof(EGLDisplay));
+    gvserInData(&config,    sizeof(EGLConfig));
+    gvserInData(&attribute, sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserOutData(value,   sizeof(EGLint));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLSurface
+eglCreateWindowSurface(EGLDisplay display, EGLConfig config, EGLNativeWindowType window, const EGLint *attribList)
+{
+    GVSERcallid callId;
+    EGLSurface  surface;
+
+    initIfNotDoneAlready();
+
+    EGLint attribListSize;
+    getAttribListSize(attribList, attribListSize);
+
+    callId = gvserCall(GVSER_EGL_CREATEWINDOWSURFACE);    
+    gvserInData(&display,   sizeof(EGLDisplay));
+    gvserInData(&config,    sizeof(EGLConfig));
+    gvserInData(&window,    sizeof(EGLNativeWindowType));
+    gvserInData(attribList, attribListSize * sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&surface, sizeof(EGLSurface));
+    gvserEndReturn();
+
+    return surface;
+}
+
+EGLSurface
+eglCreatePbufferSurface(EGLDisplay display, EGLConfig config, const EGLint *attribList)
+{
+    GVSERcallid callId;
+    EGLSurface  surface;
+
+    initIfNotDoneAlready();
+
+    EGLint attribListSize;
+    getAttribListSize(attribList, attribListSize);
+
+    callId = gvserCall(GVSER_EGL_CREATEPBUFFERSURFACE);    
+    gvserInData(&display,   sizeof(EGLDisplay));
+    gvserInData(&config,    sizeof(EGLConfig));
+    gvserInData(attribList, attribListSize * sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&surface, sizeof(EGLSurface));
+    gvserEndReturn();
+
+    return surface;
+}
+
+EGLSurface
+eglCreatePixmapSurface(EGLDisplay display, EGLConfig config, EGLNativePixmapType pixmap, const EGLint *attribList)
+{
+    GVSERcallid callId;
+    EGLSurface  surface;
+
+    initIfNotDoneAlready();
+
+    EGLint attribListSize;
+    getAttribListSize(attribList, attribListSize);
+
+    callId = gvserCall(GVSER_EGL_CREATEPIXMAPSURFACE);    
+    gvserInData(&display,   sizeof(EGLDisplay));
+    gvserInData(&config,    sizeof(EGLConfig));
+    gvserInData(&pixmap,    sizeof(EGLNativePixmapType));
+    gvserInData(attribList, attribListSize * sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&surface, sizeof(EGLSurface));
+    gvserEndReturn();
+
+    return surface;
+}
+
+EGLBoolean
+eglDestroySurface(EGLDisplay display, EGLSurface surface)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_DESTROYSURFACE);    
+    gvserInData(&display, sizeof(EGLDisplay));
+    gvserInData(&surface, sizeof(EGLSurface));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLBoolean 
+eglQuerySurface(EGLDisplay display, EGLSurface surface, EGLint attribute, EGLint *value)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_QUERYSURFACE);    
+    gvserInData(&display,   sizeof(EGLDisplay));
+    gvserInData(&surface,   sizeof(EGLSurface));
+    gvserInData(&attribute, sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserOutData(value,   sizeof(EGLint));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLBoolean
+eglBindAPI(EGLenum api)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_BINDAPI);    
+    gvserInData(&api, sizeof(EGLBoolean));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLenum
+eglQueryAPI()
+{
+    GVSERcallid callId;
+    EGLenum     api;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_QUERYAPI);    
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&api, sizeof(EGLenum));
+    gvserEndReturn();
+
+    return api;
+}
+
+EGLBoolean
+eglWaitClient(void)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_WAITCLIENT);    
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLBoolean
+eglReleaseThread(void)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_RELEASETHREAD);    
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLSurface
+eglCreatePbufferFromClientBuffer(EGLDisplay display, EGLenum bufferType, EGLClientBuffer buffer, EGLConfig config, const EGLint *attribList)
+{
+    GVSERcallid callId;
+    EGLSurface  surface;
+
+    initIfNotDoneAlready();
+
+    EGLint attribListSize;
+    getAttribListSize(attribList, attribListSize);
+
+    callId = gvserCall(GVSER_EGL_CREATEPBUFFERFROMCLIENTBUFFER);    
+    gvserInData(&display,    sizeof(EGLDisplay));
+    gvserInData(&bufferType, sizeof(EGLenum));
+    gvserInData(&buffer,     sizeof(EGLClientBuffer));
+    gvserInData(&config,     sizeof(EGLConfig));
+    gvserInData(&attribList, attribListSize * sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&surface, sizeof(EGLSurface));
+    gvserEndReturn();
+
+    return surface;
+}
+
+EGLBoolean
+eglSurfaceAttrib(EGLDisplay display, EGLSurface surface, EGLint attribute, EGLint value)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_SURFACEATTRIB);
+    gvserInData(&display,   sizeof(EGLDisplay));
+    gvserInData(&surface,   sizeof(EGLSurface));
+    gvserInData(&attribute, sizeof(EGLint));
+    gvserInData(&value,     sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLBoolean
+eglBindTexImage(EGLDisplay display, EGLSurface surface, EGLint buffer)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_BINDTEXIMAGE);
+    gvserInData(&display, sizeof(EGLDisplay));
+    gvserInData(&surface, sizeof(EGLSurface));
+    gvserInData(&buffer,  sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLBoolean
+eglReleaseTexImage(EGLDisplay display, EGLSurface surface, EGLint buffer)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_RELEASETEXIMAGE);
+    gvserInData(&display, sizeof(EGLDisplay));
+    gvserInData(&surface, sizeof(EGLSurface));
+    gvserInData(&buffer,  sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLBoolean
+eglSwapInterval(EGLDisplay display, EGLint interval)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_SWAPINTERVAL);
+    gvserInData(&display,  sizeof(EGLDisplay));
+    gvserInData(&interval, sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLContext
+eglCreateContext(EGLDisplay display, EGLConfig config, EGLContext shareContext, const EGLint *attribList)
+{
+    GVSERcallid callId;
+    EGLContext  context;
+
+    initIfNotDoneAlready();
+
+    EGLint attribListSize;
+    getAttribListSize(attribList, attribListSize);
+
+    callId = gvserCall(GVSER_EGL_CREATECONTEXT);
+    gvserInData(&display,      sizeof(EGLDisplay));
+    gvserInData(&config,       sizeof(EGLConfig));
+    gvserInData(&shareContext, sizeof(EGLContext));
+    gvserInData(&attribList,   attribListSize * sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&context, sizeof(EGLContext));
+    gvserEndReturn();
+
+    return context;
+}
+
+EGLBoolean
+eglDestroyContext(EGLDisplay display, EGLContext context)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_DESTROYCONTEXT);
+    gvserInData(&display, sizeof(EGLDisplay));
+    gvserInData(&context, sizeof(EGLContext));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLBoolean
+eglMakeCurrent(EGLDisplay display, EGLSurface drawSurface, EGLSurface readSurface, EGLContext context)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_MAKECURRENT);
+    gvserInData(&display,     sizeof(EGLDisplay));
+    gvserInData(&drawSurface, sizeof(EGLSurface));
+    gvserInData(&readSurface, sizeof(EGLSurface));
+    gvserInData(&context,     sizeof(EGLContext));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLContext eglGetCurrentContext()
+{
+    GVSERcallid callId;
+    EGLContext  context;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_GETCURRENTCONTEXT);
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&context, sizeof(EGLContext));
+    gvserEndReturn();
+
+    return context;
+}
+
+EGLSurface eglGetCurrentSurface(EGLint readdraw)
+{
+    GVSERcallid callId;
+    EGLSurface  surface;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_GETCURRENTSURFACE);
+    gvserInData(&readdraw, sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&surface, sizeof(EGLSurface));
+    gvserEndReturn();
+
+    return surface;
+}
+
+EGLDisplay eglGetCurrentDisplay()
+{
+    GVSERcallid callId;
+    EGLDisplay  display;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_GETCURRENTDISPLAY);
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&display, sizeof(EGLDisplay));
+    gvserEndReturn();
+
+    return display;
+}
+
+EGLBoolean eglQueryContext(EGLDisplay display, EGLContext context, EGLint attribute, EGLint *value)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_QUERYCONTEXT);
+    gvserInData(&display,   sizeof(EGLDisplay));
+    gvserInData(&context,   sizeof(EGLContext));
+    gvserInData(&attribute, sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserOutData(value,   sizeof(EGLint));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLBoolean
+eglWaitGL()
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_WAITGL);
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLBoolean
+eglWaitNative(EGLint engine)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_WAITNATIVE);
+    gvserInData(&engine, sizeof(EGLint));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLBoolean
+eglSwapBuffers(EGLDisplay display, EGLSurface surface)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_SWAPBUFFERS);
+    gvserInData(&display, sizeof(EGLDisplay));
+    gvserInData(&surface, sizeof(EGLSurface));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserEndReturn();
+
+    return status;
+}
+
+EGLBoolean
+eglCopyBuffers(EGLDisplay display, EGLSurface surface, EGLNativePixmapType target)
+{
+    GVSERcallid callId;
+    EGLBoolean  status;
+
+    initIfNotDoneAlready();
+
+    callId = gvserCall(GVSER_EGL_COPYBUFFERS);
+    gvserInData(&display, sizeof(EGLDisplay));
+    gvserInData(&surface, sizeof(EGLSurface));
+    gvserInData(&target,  sizeof(EGLNativePixmapType));
+    gvserEndCall();
+
+    gvserReturn(callId);
+    gvserOutData(&status, sizeof(EGLBoolean));
+    gvserEndReturn();
+
+    return status;
 }
